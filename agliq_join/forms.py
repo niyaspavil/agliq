2f946f32658691eb349241fc2fe5663d6f96c068
niyaspavil <pavilniyas@gmail.com>
2014-11-07 13:23:39 +0530

    edited form field names

--- agliq_join/forms.py
+++ agliq_join/forms.py
@@ -4,9 +4,9 @@ from django import forms
 class UploadFileForm(forms.Form):
     first_name = forms.CharField(max_length = 50)
     last_name = forms.CharField(max_length = 50)
-    project_url = forms.URLField(max_length = 200, help_text = 'Please enter the Project URL.')
+    projects_url = forms.URLField(max_length = 200, help_text = 'Please enter the Project URL.')
     code_url = forms.URLField(max_length = 200, help_text = 'Please enter the code URL.')
-    file = forms.FileField(
+    resume = forms.FileField(
         label='Select a file',
         help_text='max. 42 megabytes'
     )
